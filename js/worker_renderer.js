bd014d16be9c5c71c330eff2715c7ddcc42aeea0
yariktel54 <108540063+yariktel54@users.noreply.github.com>
2026-02-27 19:30:36 +0200

    Deleted for next redesigned ver.

--- js/worker_renderer.js
+++
@@ -1,8 +0,0 @@
-// js/worker_renderer.js
-import { render_image } from './renderer.js';
-
-self.onmessage = (ev) => {
-    const { atoms, opts } = ev.data;
-    const out = render_image(atoms, opts); // Uint8ClampedArray
-    self.postMessage({ frame: out, w: opts.img_size[1], h: opts.img_size[0] }, [out.buffer]);
-};
